8b67121e2a38c022b157ea67873ef067df73f55b
Prince-soni <princesoni8761969@gmail.com>
2026-09-04 16:20:07 +0530

    make auth frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import Landing from './pages/landing/landing'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/landing/landing";
+import Login from "./pages/auth/Login";
+import Registration from "./pages/auth/Registration";
 
 function App() {
-  return <Landing />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
